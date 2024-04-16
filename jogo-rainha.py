f0214c5b8218a8d52dabf4ba89639be5b31c1cf5
Bruna Veloso <91560228+gamesbrunaa@users.noreply.github.com>
2024-04-16 20:56:43 -0300

    update

--- jogo-rainha.py
+++ jogo-rainha.py
@@ -1,41 +1,43 @@
-def verifica_ataque(rainhas):
-    for i in range(len(rainhas)):
-        for j in range(i + 1, len(rainhas)):
-            x1, y1 = rainhas[i]
-            x2, y2 = rainhas[j]
-            if x1 == x2 or y1 == y2 or abs(x1 - x2) == abs(y1 - y2):
-                return True
-    return False
-
-def dfs(linha, rainhas):
-    if linha == 8:
+def dfs(linha, rainhas, size):
+    if linha == size:
         return [rainhas.copy()]
 
     solutions = []
-    for coluna in range(8):
+    for coluna in range(size):
         rainha = (linha, coluna)
-        rainhas.append(rainha)
-        if not verifica_ataque(rainhas):
-            for child in dfs(linha + 1, rainhas):
+        if not tem_ataque(rainhas, rainha):  
+            rainhas.append(rainha)
+            for child in dfs(linha + 1, rainhas, size): 
                 solutions.append(child)
-        rainhas.pop()
+            rainhas.pop()
 
     return solutions
 
-def mostra_tabuleiro(rainhas):
-    tabuleiro = [['.' for _ in range(8)] for _ in range(8)]
+def tem_ataque(rainhas, nova_rainha):
     for rainha in rainhas:
-        tabuleiro[rainha[0]][rainha[1]] = 'X'
-    for linha in tabuleiro:
-        print(' '.join(linha))
+        if rainha[0] == nova_rainha[0] or rainha[1] == nova_rainha[1] or abs(rainha[0] - nova_rainha[0]) == abs(rainha[1] - nova_rainha[1]):
+            return True
+    return False
+
+def mostra_tabuleiro(rainhas, size):
+    for i in range(size):
+        linha = ''
+        for j in range(size):
+            if (i, j) in rainhas:
+                linha += 'X '
+            else:
+                linha += '. '
+        print(linha)
+
 
 def main():
+    size = 12;
     rainhas = []
-    solutions = dfs(0, rainhas)
+    solutions = dfs(0, rainhas, size)
 
     for i, sol in enumerate(solutions):
         print(f'Solução {i + 1}:')
-        mostra_tabuleiro(sol)
+        mostra_tabuleiro(sol, size)
         print()
 
 main()
